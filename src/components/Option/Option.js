85e3172665b02a8efda838ede42af62d53a882f6
Kyle Haining <hainingk@cs.washington.edu>
2024-05-13 14:16:17 -0700

    Option component scaffolding

---
+++ src/components/Option/Option.js
@@ -0,0 +1,16 @@
+import './Option.css';
+
+const DISPLAY_NAME = 'option';
+
+function Option({ id, name, value}) {
+  return (
+    <input
+      type="checkbox"
+      id={id}
+      name={name}
+      value={value}
+    />
+  );
+}
+
+export default Option;
