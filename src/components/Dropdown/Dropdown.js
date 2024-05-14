78d0ac331382ed926ee2eb74705c90438c12c012
Kyle Haining <hainingk@cs.washington.edu>
2024-05-13 17:29:56 -0700

    Open and close menu via click

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import { Option } from 'components';
+import classNames from 'classnames';
 import './Dropdown.css';
 
 const DISPLAY_NAME = 'dropdown';
 
 function Dropdown({ id, name, value, children}) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = (e) => {
+
+    if (!showMenu) {
+      setShowMenu(true);
+      e.stopPropagation();
+      document.addEventListener('click', () => {
+        setShowMenu(false);
+        console.log('clicked');
+      }, { once: true });
+    }
+  };
+
   return (
     <>
-      <div className={`${DISPLAY_NAME}-select`}>
+      <div
+        className={`${DISPLAY_NAME}-select`}
+        onClick={toggleMenu}
+      >
         Select
       </div>
-      <div className={`${DISPLAY_NAME}-menu`}>
+      <div className={classNames(`${DISPLAY_NAME}-menu`, { hide: !showMenu })}>
         {children}
       </div>
     </>
