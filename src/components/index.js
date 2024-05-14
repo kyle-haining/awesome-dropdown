66dede14fd344b3be0001d07868749472dc875aa
Kyle Haining <hainingk@cs.washington.edu>
2024-05-14 11:31:48 -0700

    Remove no longer used multi components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 export { Checkbox } from './Checkbox';
 export { Option } from './Option';
 export { Menu } from './Menu';
-export { SelectMenu } from './SelectMenu';
 export { Dropdown } from './Dropdown';
-export { SelectDropdown } from './SelectDropdown';
